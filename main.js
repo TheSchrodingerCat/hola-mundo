1955ea5d44fcc50b413a9f9c1407e78d132399a7
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-16 14:15:13 -0400

    se agrega window.onload al principio y se situa dentro el addEventListener del boton. También se crea otra div para poner el anuncio

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+window.onload = function(){
+	var botoncito = document.getElementById("demo");
+	botoncito.addEventListener("click",saludar);
+}
 
 function saludar(){
-	var anuncio = document.getElementById("mi-div");
+	var anuncio = document.getElementById("anuncio");
 	anuncio.innerHTML = "Hola Mundo"
 }
